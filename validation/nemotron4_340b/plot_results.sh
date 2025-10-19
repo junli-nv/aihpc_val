ca46aeb87245264ab2ffe455948b01ab8d8d5cbb
junli-nv <junliz@nvidia.com>
2025-10-19 21:20:59 +0800

    Update plot_results.sh

--- validation/nemotron4_340b/plot_results.sh
+++ validation/nemotron4_340b/plot_results.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-plot(){
+apt install -y gnuplot-nox
+
+# SACCT_FORMAT="User,JobID,Start,End,Elapsed,AllocNodes,WorkDir%30,NodeList%100" sacct -j ${JOBID}
+
+plot_jid(){
   jid=$1
   logs=($(ls -1 $(dirname $(sacct -o 'StdOut%1000' -j $jid|grep -E '.txt|.out|.log'))/*${jid}*{.txt,.log,.out} 2>/dev/null))
   for i in ${logs[*]}; do
@@ -10,17 +14,16 @@ plot(){
   done
 }
 
-SACCT_FORMAT="User,JobID,Start,End,Elapsed,AllocNodes,WorkDir%30,NodeList%100" sacct -j ${JOBID}
+plot_dir(){
+  logdir=$1
+  cd ${logdir}
+  logs=($(ls -1 *.txt *.out *.log))
+  for i in ${logs[*]}; do
+    log=$i
+    if [ $(grep iteration.*train_step_timing $log|wc -l) -gt 10 ]; then
+      awk -F'|' '/iteration.*train_step_timing/{print $8}' $log|awk '{print $NF}' \
+        | gnuplot -e "set title '$(basename $log|tr '_' '+')'; set term dumb size 300,50; set yrange [0:10]; set ytics 0.5; plot '-' with   linespoints"
+    fi
+  done
+}
 
-apt install -y gnuplot-nox
-
-logs=(
-$(ls -1 *.txt *.out)
-)
-for i in ${logs[*]}; do
-  log=$i
-  if [ $(grep iteration.*train_step_timing $log|wc -l) -gt 10 ]; then
-    awk -F'|' '/iteration.*train_step_timing/{print $8}' $log|awk '{print $NF}' \
-      | gnuplot -e "set title '$(basename $log|tr '_' '+')'; set term dumb size 300,50; set yrange [0:10]; set ytics 0.5; plot '-' with linespoints"
-  fi
-done
