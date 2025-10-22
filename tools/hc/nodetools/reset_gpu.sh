5f7b520a25dc1a7a44f2b6ca526dee9a9aba3ca0
junli-nv <junliz@nvidia.com>
2025-10-22 22:47:25 +0800

    Update reset_gpu.sh

--- tools/hc/nodetools/reset_gpu.sh
+++ tools/hc/nodetools/reset_gpu.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+kill -9 $(nvidia-smi --query-compute-apps=pid --format=csv,noheader,nounits) &>/dev/null || true
 systemctl stop cmd munge.service slurmd.service nvidia-persistenced.service nvidia-dcgm.service nvidia-imex.service
 nvidia-smi -r
 systemctl start cmd munge.service slurmd.service nvidia-persistenced.service nvidia-dcgm.service nvidia-imex.service
