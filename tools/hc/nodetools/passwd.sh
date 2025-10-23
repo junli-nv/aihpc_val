7aa588553329b9b277e5f9b4ba5bd91c31d58e03
junli-nv <junliz@nvidia.com>
2025-10-23 10:49:20 +0800

    Update passwd.sh

--- tools/hc/nodetools/passwd.sh
+++ tools/hc/nodetools/passwd.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 #
-#export bmc_username=admin
 export bmc_username=root
 export bmc_password=GB200@pbd
+
+export nvswitch_os_user=admin
+export nvswitch_os_pass=Nvidia@123
+export nvswitch_bmc_user=root
+export nvswitch_bmc_pass=JulietBmc@123
+
+export powershelf_bmc_user=root
+export powershelf_bmc_pass=0penBmc
