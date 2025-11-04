04e2cb6c7364d94a31f54c30f1f50096b8bd457e
junli-nv <junliz@nvidia.com>
2025-11-04 14:08:39 +0800

    Update checker.sh

--- tools/hc/checker.sh
+++ tools/hc/checker.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+extra_check=0
+dmesg_hours_to_look_back=8
+nvme_disk_expected_count=9
+nvme_data_disk_expected_count=8
+nvme_data_disk_expected_size_tb=7
+nvme_os_disk_expected_count=1
+nvme_os_disk_expected_size_tb=1
+
+check_cpu=1
+cpu_expected_count=144
+
+check_mem=1
+memory_expected_size_gb_per_socket=480
+
+check_gpu=1
+gpu_expected_count=4
+gpu_expected_memory_size_gb=32
+check_nvlink=1
+check_imex=1
+check_cuda=1
+
+check_ib=1
+ib_expected_count=4
+bf3_expected_ethernet_count=4
+
+check_spx=0
+
+check_nfs=1
+
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 topdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
@@ -15,7 +44,6 @@ set_cpu_freq_userspace(){
 global_status=0
 reason=""
 
-extra_check=0
 dry_run=0
 options=d,e,h
 optionl=dry-run,--extra-check,help
@@ -49,27 +77,27 @@ done
 
 if [ $extra_check -ne 0 ]; then
   ##AER
-  if [ $(dmesg --since '1 hour ago' | grep 'pcieport.*AER:'|wc -l) -ne 0 ]; then
+  if [ $(dmesg --since "${dmesg_hours_to_look_back} hour ago" | grep 'pcieport.*AER:'|wc -l) -ne 0 ]; then
       msg='WARN: pcieport AER shown in dmesg'
       echo $msg
   fi
   ##NV_ERR_INVALID_STATE
-  if [ $(dmesg --since '8 hour ago' | grep NV_ERR_INVALID_STATE|wc -l) -ne 0 ]; then
+  if [ $(dmesg --since "${dmesg_hours_to_look_back} hour ago" | grep NV_ERR_INVALID_STATE|wc -l) -ne 0 ]; then
       msg='ERROR: NV_ERR_INVALID_STATE shown in dmesg'
       echo $msg
   fi
   ## Hardware Error
-  if [ $(dmesg --since '8 hour ago' | grep 'Hardware Error'|wc -l) -ne 0 ]; then
+  if [ $(dmesg --since "${dmesg_hours_to_look_back} hour ago" | grep 'Hardware Error'|wc -l) -ne 0 ]; then
       msg='ERROR: Hardware Error shown in dmesg'
       echo $msg
   fi
   ## DBE
-  if [ $(dmesg --since '8 hour ago' | grep 'Xid.*uncorrectable double bit error'|wc -l) -ne 0 ]; then
+  if [ $(dmesg --since "${dmesg_hours_to_look_back} hour ago" | grep 'Xid.*uncorrectable double bit error'|wc -l) -ne 0 ]; then
       msg='ERROR: uncorrectable double bit error shown in dmesg'
       echo $msg
   fi
   ## knvlinkDiscoverPostRxDetLinks
-  if [ $(dmesg --since '8 hour ago' | grep 'NVRM: knvlinkDiscoverPostRxDetLinks'|wc -l) -ne 0 ]; then
+  if [ $(dmesg --since "${dmesg_hours_to_look_back} hour ago" | grep 'NVRM: knvlinkDiscoverPostRxDetLinks'|wc -l) -ne 0 ]; then
       msg='ERROR: knvlinkDiscoverPostRxDetLinks error shown in dmesg'
       echo $msg
   fi
@@ -79,22 +107,21 @@ if [ $extra_check -ne 0 ]; then
   echo $i=$[$(blockdev --getsz /dev/${i}n[0-9])*512/1000/1000/1000/1000]
   done
   ))
-  a=$(echo ${ret[*]}|tr ' ' '\n'|grep nvme.*=7|wc -l)
-  b=$(echo ${ret[*]}|tr ' ' '\n'|grep nvme.*=1|wc -l)
-  if [ ${#ret[*]} -ne 9 ]; then
-    msg='WARN: NVME disk number is not 9'
+  a=$(echo ${ret[*]}|tr ' ' '\n'|grep nvme.*=${nvme_data_disk_expected_size_tb}|wc -l)
+  b=$(echo ${ret[*]}|tr ' ' '\n'|grep nvme.*=${nvme_os_disk_expected_size_tb}|wc -l)
+  if [ ${#ret[*]} -ne ${nvme_disk_expected_count} ]; then
+    msg="WARN: NVME disk number is not ${nvme_disk_expected_count}"
     echo $msg
   else
-    if [ $a -ne 8 ]; then
-      msg='WARN: NVME data disk number is not 8'
+    if [ $a -ne ${nvme_data_disk_expected_count} ]; then
+      msg="WARN: NVME data disk number is not ${nvme_data_disk_expected_count}"
       echo $msg
     fi
-    if [ $b -ne 1 ]; then
-      msg='WARN: NVME os disk number is not 1'
+    if [ $b -ne ${nvme_os_disk_expected_count} ]; then
+      msg="WARN: NVME os disk number is not ${nvme_os_disk_expected_count}"
       echo $msg
     fi
   fi
-  
   ##Enroot disk check
   enroot_runtime_path=$(grep ENROOT_RUNTIME_PATH /etc/enroot/enroot.conf|grep -o /.*/)
   if [ ! -d $enroot_runtime_path ]; then
@@ -120,170 +147,191 @@ if [ $extra_check -ne 0 ]; then
   fi
 fi
 
-## CPU
-if [ $(grep processor /proc/cpuinfo|wc -l) -ne 144 ]; then
-  msg='ERROR: CPU CORES is not 144'
-  echo $msg
-  reason="${reason} ${msg}"
-  global_status=$[global_status+1]
-fi
-cpupower -c all frequency-set -g performance &>/dev/null
-if [ $(grep -v performance /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor|wc -l) -ne 0 ]; then
-  msg='ERROR: CPU governor is not performance'
-  echo $msg
-  reason="${reason} ${msg}"
-  global_status=$[global_status+1]
-fi
-s0=$(cat /sys/devices/system/cpu/cpu{0..71}/cpufreq/cpuinfo_cur_freq|awk 'BEGIN{sum=0}{sum+=$1}END{print int(sum/NR/1000)}')
-s1=$(cat /sys/devices/system/cpu/cpu{72..143}/cpufreq/cpuinfo_cur_freq|awk 'BEGIN{sum=0}{sum+=$1}END{print int(sum/NR/1000)}')
-if [ ${s0} -lt 500 ]; then
-  msg='ERROR: CPU socket0 average frequency is abnormal(<500MHz)'
-  echo $msg
-  reason="${reason} ${msg}"
-  global_status=$[global_status+1]
-fi
-if [ ${s1} -lt 500 ]; then
-  msg='ERROR: CPU socket1 average frequency is abnormal(<500MHz)'
-  echo $msg
-  reason="${reason} ${msg}"
-  global_status=$[global_status+1]
-fi
-
-## MEM
-if [ $(dmidecode -t memory|grep -P '\tSize:'|tr -d '\t'|grep 480|wc -l) -ne 2 ]; then
-  msg='ERROR: Memory size is not 2*480GB'
-  echo $msg
-  reason="${reason} ${msg}"
-  global_status=$[global_status+1]
-fi
-
-## GPU
-if [ $(lspci | grep '3D controller' | wc -l) -ne 4 ]; then
-  msg='ERROR: GPU number is not 4'
-  echo $msg
-  reason="${reason} ${msg}"
-  global_status=$[global_status+1]
-fi
-if [ $(lspci | grep '3D controller' | grep 'rev ff' | wc -l) -gt 0 ]; then
-  msg='ERROR: GPU met ref ff issue'
-  echo $msg
-  reason="${reason} ${msg}"
-  global_status=$[global_status+1]
-fi
-if `nvidia-smi -L` &>/dev/null; then
-  msg="ERROR: GPU driver didn't load correctly"
-  echo $msg
-  reason="${reason} ${msg}"
-  global_status=$[global_status+1]
-fi
-if [ $(nvidia-smi nvlink -s|grep '5[0-9].* GB/s'|wc -l) -ne 72 ]; then
-  msg='ERROR: 72 GPU NVLINKs are not fully connected'
-  echo $msg
-  reason="${reason} ${msg}"
-  global_status=$[global_status+1]
-fi
-if [ $(nvidia-smi --format=csv --query-gpu gpu_bus_id,fabric.status|grep 'GPU requires reset'|wc -l
-) -ne 0 ]; then
-  msg='ERROR: GPU needs reset to recover the NVLINKs'
-  echo $msg
-  reason="${reason} ${msg}"
-  global_status=$[global_status+1]
-fi
-
-## IMEX
-if [ $(ls -1 /dev/nvidia-caps-imex-channels/|wc -l) -lt 1 ]; then
-  msg='ERROR: IMEX channel doesnt exist'
-  echo $msg
-  reason="${reason} ${msg}"
-  global_status=$[global_status+1]
-fi
-if [ ! -f /etc/nvidia-imex/nodes_config.cfg ]; then
-  msg='ERROR: IMEX /etc/nvidia-imex/nodes_config.cfg doesnt exist. Restart nvidia-imex service'
-  echo $msg
-  reason="${reason} ${msg}"
-  global_status=$[global_status+1]
-fi
-if [ "X$(nvidia-imex-ctl -q 2>/dev/null)" != "XREADY" ]; then
-  msg='ERROR: IMEX is not active'
-  echo $msg
-  reason="${reason} ${msg}"
-  global_status=$[global_status+1]
-fi
-
-## IB
-if [ $(lspci | grep 'Infiniband controller'|wc -l) -ne 4 ]; then
-  msg='ERROR: IB HCA number is not 4'
-  echo $msg
-  reason="${reason} ${msg}"
-  global_status=$[global_status+1]
-fi
-if [ $(lspci | grep 'Ethernet.*BlueField-3'|wc -l) -ne 4 ]; then
-  msg='WARN: BlueField-3 ethernet ports number is not 4'
-  echo $msg
-  #reason="${reason} ${msg}"
-  #global_status=$[global_status+1]
-fi
-active_hcas=($(lspci -D|grep 'Infiniband controller'|awk '{print $1}'|while read i; do hca=$(basename $(ls -l /sys/class/infiniband|grep -o ${i}.*) 2>/dev/null); [ ! -z $hca ] && grep ACTIVE /sys/class/infiniband/${hca}/ports/1/state &>/dev/null && echo ${hca}:1;done))
-if [ ${#active_hcas[*]} -ne 4 ]; then
-  msg="WARNING: Active HCA number is not 4" # $(echo ${active_hcas[*]}|tr ' ' ',')
-  echo $msg
-  reason="${reason} ${msg}"
-  global_status=$[global_status+1]
-fi
-hcas=($(lspci -D|grep 'Infiniband controller'|awk '{print $1}'|while read i; do echo $(basename $(ls -l /sys/class/infiniband|grep -o ${i}.*) 2>/dev/null);done))
-ret=($(
-for dev in ${hcas[*]}; do
-  link_downed=$(</sys/class/infiniband/${dev}/ports/1/counters/link_downed)
-  symbol_error=$(</sys/class/infiniband/${dev}/ports/1/counters/symbol_error)
-  if [[ ${link_downed} -ne 0 || ${symbol_error} -ne 0 ]]; then
-    echo ${dev}:link_downed=${link_downed},symbol_error=${symbol_error}
+if [ ${check_cpu} -ne 0 ]; then
+  ## CPU
+  if [ $(grep processor /proc/cpuinfo|wc -l) -ne ${cpu_expected_count} ]; then
+    msg="ERROR: CPU CORES is not ${cpu_expected_count}"
+    echo $msg
+    reason="${reason} ${msg}"
+    global_status=$[global_status+1]
   fi
-done
-))
-if [ ${#ret[*]} -ne 0 ]; then
-  echo "WARNING: ${ret[*]}"
-fi
-ret=($(
-for dev in ${hcas[*]}; do
-  link_w=($(mlxlink -d ${dev} --port_type pcie | grep Width | awk '{print $(NF-1)}'))
-  if [[ ${link_w[0]} != "16X" ]]; then
-    echo "${dev}[LinkWidth]:${link_w[0]}"
+  cpupower -c all frequency-set -g performance &>/dev/null
+  if [ $(grep -v performance /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor|wc -l) -ne 0 ]; then
+    msg='ERROR: CPU governor is not performance'
+    echo $msg
+    reason="${reason} ${msg}"
+    global_status=$[global_status+1]
   fi
-done
-))
-if [ ${#ret[*]} -ne 0 ]; then
-  echo "WARNING: ${ret[*]}"
+  s0=$(cat /sys/devices/system/node/node0/cpu*/cpufreq/cpuinfo_cur_freq|awk 'BEGIN{sum=0}{sum+=$1}END{print int(sum/NR/1000)}')
+  s1=$(cat /sys/devices/system/node/node1/cpu*/cpufreq/cpuinfo_cur_freq|awk 'BEGIN{sum=0}{sum+=$1}END{print int(sum/NR/1000)}')
+  if [ ${s0} -lt 500 ]; then
+    msg='ERROR: CPU socket0 average frequency is abnormal(<500MHz)'
+    echo $msg
+    reason="${reason} ${msg}"
+    global_status=$[global_status+1]
+  fi
+  if [ ${s1} -lt 500 ]; then
+    msg='ERROR: CPU socket1 average frequency is abnormal(<500MHz)'
+    echo $msg
+    reason="${reason} ${msg}"
+    global_status=$[global_status+1]
+  fi
 fi
 
-## NFS
-if `df -Th|grep master:/home` &>/dev/null; then
-  msg='ERROR: home not mounted'
-  echo $msg
-  reason="${reason} ${msg}"
-  global_status=$[global_status+1]
-fi
-if `df -Th|grep master:/cm/shared` &>/dev/null; then
-  msg='ERROR: cmshared not mounted'
-  echo $msg
-  reason="${reason} ${msg}"
-  global_status=$[global_status+1]
+if [ ${check_mem} -ne 0 ]; then
+  ## MEM
+  if [ $(dmidecode -t memory|grep -P '\tSize:'|tr -d '\t'|grep ${memory_expected_size_gb_per_socket}|wc -l) -ne 2 ]; then
+    msg="ERROR: Memory size is not 2*${memory_expected_size_gb_per_socket}GB"
+    echo $msg
+    reason="${reason} ${msg}"
+    global_status=$[global_status+1]
+  fi
 fi
 
-#CUDA Test
-if [ $(nvidia-smi topo -p2p n|grep GPU[0-9].*OK|wc -l) -eq 0 ]; then
+if [ ${check_gpu} -ne 0 ]; then
+  ## GPU
+  if [ $(lspci | grep '3D controller' | wc -l) -ne ${gpu_expected_count} ]; then
+    msg="ERROR: GPU number is not ${gpu_expected_count}"
+    echo $msg
+    reason="${reason} ${msg}"
+    global_status=$[global_status+1]
+  fi
+  if [ $(lspci | grep '3D controller' | grep 'rev ff' | wc -l) -gt 0 ]; then
+    msg='ERROR: GPU met ref ff issue'
+    echo $msg
+    reason="${reason} ${msg}"
+    global_status=$[global_status+1]
+  fi
+  if `nvidia-smi -L` &>/dev/null; then
+    msg="ERROR: GPU driver didn't load correctly"
+    echo $msg
+    reason="${reason} ${msg}"
+    global_status=$[global_status+1]
+  fi
+fi
+
+if [ ${check_nvlink} -ne 0 ]; then
+  if [ $(nvidia-smi nvlink -s|grep '5[0-9].* GB/s'|wc -l) -ne 72 ]; then
+    msg='ERROR: 72 GPU NVLINKs are not fully connected'
+    echo $msg
+    reason="${reason} ${msg}"
+    global_status=$[global_status+1]
+  fi
+  if [ $(nvidia-smi --format=csv --query-gpu gpu_bus_id,fabric.status|grep 'GPU requires reset'|wc -l
+  ) -ne 0 ]; then
+    msg='ERROR: GPU needs reset to recover the NVLINKs'
+    echo $msg
+    reason="${reason} ${msg}"
+    global_status=$[global_status+1]
+  fi
+  if [ $(nvidia-smi topo -p2p n|grep GPU[0-9].*OK|wc -l) -eq 0 ]; then
     msg='ERROR: GPU P2P is disabled'
     echo $msg
     reason="${reason} ${msg}"
     global_status=$[global_status+1]
+  fi
 fi
-if [ -x ${topdir}/cudatest/foo ]; then
-  timeout 3 ${topdir}/cudatest/foo &>/dev/null
-  ret=$?
-  if [ $ret -ne 0 ]; then
-    msg='ERROR: Cannot run even a simple CUDA app'
+
+if [ ${check_imex} -ne 0 ]; then
+  ## IMEX
+  if [ $(ls -1 /dev/nvidia-caps-imex-channels/|wc -l) -lt 1 ]; then
+    msg='ERROR: IMEX channel doesnt exist'
     echo $msg
     reason="${reason} ${msg}"
     global_status=$[global_status+1]
+  fi
+  if [ ! -f /etc/nvidia-imex/nodes_config.cfg ]; then
+    msg='ERROR: IMEX /etc/nvidia-imex/nodes_config.cfg doesnt exist. Restart nvidia-imex service'
+    echo $msg
+    reason="${reason} ${msg}"
+    global_status=$[global_status+1]
+  fi
+  if [ "X$(nvidia-imex-ctl -q 2>/dev/null)" != "XREADY" ]; then
+    msg='ERROR: IMEX is not active'
+    echo $msg
+    reason="${reason} ${msg}"
+    global_status=$[global_status+1]
+  fi
+fi
+
+if [ ${check_ib} -ne 0 ]; then
+  ## IB
+  if [ $(lspci | grep 'Infiniband controller'|wc -l) -ne ${ib_expected_count} ]; then
+    msg="ERROR: IB HCA number is not ${ib_expected_count}"
+    echo $msg
+    reason="${reason} ${msg}"
+    global_status=$[global_status+1]
+  fi
+  if [ $(lspci | grep 'Ethernet.*BlueField-3'|wc -l) -ne ${bf3_expected_ethernet_count} ]; then
+    msg="WARN: BlueField-3 ethernet ports number is not ${bf3_expected_ethernet_count}"
+    echo $msg
+    #reason="${reason} ${msg}"
+    #global_status=$[global_status+1]
+  fi
+  active_hcas=($(lspci -D|grep 'Infiniband controller'|awk '{print $1}'|while read i; do hca=$(basename $(ls -l /sys/class/infiniband|grep -o ${i}.*) 2>/dev/null); [ ! -z $hca ] && grep ACTIVE /sys/class/infiniband/${hca}/ports/1/state &>/  dev/null && echo ${hca}:1;done))
+  if [ ${#active_hcas[*]} -ne ${ib_expected_count} ]; then
+    msg="WARNING: Active HCA number is not ${ib_expected_count}" # $(echo ${active_hcas[*]}|tr ' ' ',')
+    echo $msg
+    reason="${reason} ${msg}"
+    global_status=$[global_status+1]
+  fi
+  hcas=($(lspci -D|grep 'Infiniband controller'|awk '{print $1}'|while read i; do echo $(basename $(ls -l /sys/class/infiniband|grep -o ${i}.*) 2>/dev/null);done))
+  ret=($(
+  for dev in ${hcas[*]}; do
+    link_downed=$(</sys/class/infiniband/${dev}/ports/1/counters/link_downed)
+    symbol_error=$(</sys/class/infiniband/${dev}/ports/1/counters/symbol_error)
+    if [[ ${link_downed} -ne 0 || ${symbol_error} -ne 0 ]]; then
+      echo ${dev}:link_downed=${link_downed},symbol_error=${symbol_error}
+    fi
+  done
+  ))
+  if [ ${#ret[*]} -ne 0 ]; then
+    echo "WARNING: ${ret[*]}"
+  fi
+  ret=($(
+  for dev in ${hcas[*]}; do
+    link_w=($(mlxlink -d ${dev} --port_type pcie | grep Width | awk '{print $(NF-1)}'))
+    if [[ ${link_w[0]} != "16X" ]]; then
+      echo "${dev}[LinkWidth]:${link_w[0]}"
+    fi
+  done
+  ))
+  if [ ${#ret[*]} -ne 0 ]; then
+    echo "WARNING: ${ret[*]}"
+  fi
+fi
+
+if [ ${check_spx} -ne 0 ]; then
+## SPX
+fi
+
+if [ ${check_nfs} -ne 0 ]; then
+  ## NFS
+  if `df -Th|grep master:/home` &>/dev/null; then
+    msg='ERROR: home not mounted'
+    echo $msg
+    reason="${reason} ${msg}"
+    global_status=$[global_status+1]
+  fi
+  if `df -Th|grep master:/cm/shared` &>/dev/null; then
+    msg='ERROR: cmshared not mounted'
+    echo $msg
+    reason="${reason} ${msg}"
+    global_status=$[global_status+1]
+  fi
+fi
+
+if [ ${check_cuda} -ne 0 ]; then
+  #CUDA Test
+  if [ -x ${topdir}/cudatest/foo ]; then
+    timeout 3 ${topdir}/cudatest/foo &>/dev/null
+    ret=$?
+    if [ $ret -ne 0 ]; then
+      msg='ERROR: Cannot run even a simple CUDA app'
+      echo $msg
+      reason="${reason} ${msg}"
+      global_status=$[global_status+1]
+    fi
   fi
 fi
 
