3c4540fc0b749098a6477849b40bcb3c68ebecd3
junli-nv <junliz@nvidia.com>
2025-11-10 16:45:28 +0800

    Update ibcounter.sh

--- tools/hc/ibcounter.sh
+++ tools/hc/ibcounter.sh
@@ -3,11 +3,22 @@
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin
 
 clear(){
-  lspci -D|grep 'Infiniband controller'|awk '{print $1}'|while read i; do echo $(basename $(ls -l /sys/class/infiniband|grep -o ${i}.*));done|while read dev in ${hcas[*]}; do mlxlink -d $dev -p 1 -pc &>/dev/null; done
+  hcas=($(lspci -D|grep 'Infiniband controller'|awk '{print $1}'|while read i; do echo $(basename $(ls -l /sys/class/infiniband|grep -o ${i}.*));done))
+  for dev in "${hcas[@]}"; do
+    mlxlink -d $dev -p 1 -pc &>/dev/null
+  done
 }
 
 query(){
-lspci -D|grep 'Infiniband controller'|awk '{print $1}'|while read i; do echo $(basename $(ls -l /sys/class/infiniband|grep -o ${i}.*));done|while read dev in ${hcas[*]}; do link_downed=$(</sys/class/infiniband/${dev}/ports/1/counters/link_downed); symbol_error=$(</sys/class/infiniband/${dev}/ports/1/counters/symbol_error); [[ ${link_downed} -ne 0 || ${symbol_error} -ne 0 ]] && echo ${dev}:link_downed=${link_downed},symbol_error=${symbol_error}; done
+  hcas=($(lspci -D|grep 'Infiniband controller'|awk '{print $1}'|while read i; do echo $(basename $(ls -l /sys/class/infiniband|grep -o ${i}.*));done))
+  for dev in "${hcas[@]}"; do
+    link_downed=$(</sys/class/infiniband/${dev}/ports/1/counters/link_downed)
+    symbol_error=$(</sys/class/infiniband/${dev}/ports/1/counters/symbol_error)
+    #mlxlink -d ${dev} -c
+    phys_state=$(cat /sys/class/infiniband/${dev}/ports/1/phys_state|tr -d ' ')
+    stat=$(cat /sys/class/infiniband/${dev}/ports/1/state|tr -d ' ')
+    echo ${dev}:phys_state=${phys_state},stat=${stat},link_downed=${link_downed},symbol_error=${symbol_error}
+  done
 }
 
 action=$1
