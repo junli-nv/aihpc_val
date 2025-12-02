c6be2b902ca304598a574c530932017eb70d4e55
junli-nv <junliz@nvidia.com>
2025-12-02 17:23:47 +0800

    Update gen_hosts.sh

--- tools/hc/nodetools/gen_hosts.sh
+++ tools/hc/nodetools/gen_hosts.sh
@@ -4,14 +4,11 @@ node_prefix="gb200"
 sw_prefix="nvsw"
 ps_prefix="ps"
 
-cmsh <<- EOF | grep -v PhysicalNode| tee hosts.list
-device; foreach -t PhysicalNode ( get hostname; interfaces; list bmc)|grep -E "${node_prefix}-|bmc"|paste - -|awk '{print \$1,\$4}'
-EOF
+cmsh -c 'device; foreach -t PhysicalNode ( get hostname; interfaces; list bmc)' \
+  | grep -E "${node_prefix}-|bmc" | paste - - | awk '{print $1,$4}' | tee hosts.list
 
-cmsh <<- EOF | grep -v Switch| tee -a hosts.list
-device; foreach -t Switch ( get hostname; interfaces; list bmc)|grep -E "${sw_prefix}-|bmc"|paste - -|awk '{print \$1,\$4}'
-EOF
+cmsh -c 'device; foreach -t Switch ( get hostname; interfaces; list bmc)' \
+  | grep -E "${sw_prefix}-|bmc" | paste - - | awk '{print $1,$4}' | tee -a hosts.list
 
-cmsh <<- EOF | grep -v PowerShelf| tee -a hosts.list
-device; foreach -t PowerShelf ( get hostname; interfaces; list bmc)|grep -E "${ps_prefix}-|bmc"|paste - -|awk '{print \$1,\$4}'
-EOF
+cmsh -c 'device; foreach -t PowerShelf ( get hostname; interfaces; list bmc)' \
+  | grep -E "${ps_prefix}-|bmc" | paste - - | awk '{print $1,$4}' | tee -a hosts.list
