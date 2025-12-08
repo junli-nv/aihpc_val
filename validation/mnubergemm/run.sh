c2a2d4f9dbec94c29b928f79afed30398c18874a
junli-nv <junliz@nvidia.com>
2025-12-08 18:49:08 +0800

    Create run.sh

---
+++ validation/mnubergemm/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+
+module load slurm
+hosts=(
+$(scontrol show hostname $(sinfo -al|grep 'reserved.*junli_val'|awk '{print $NF}'))
+)
+
+for i in {0..9}
+do
+  for h in ${hosts[*]}
+  do
+    sbatch --reservation=junli_val \
+      -N 1 \
+      -w "${h}" \
+      -t 2:30:00 \
+      --dependency=singleton \
+      --job-name=MNUBERGEMM-${h} \
+      --output=${USER}-MNUBERGEMEM-${h}-%j.txt \
+      mnubergemm.sbatch
+  done
+done
