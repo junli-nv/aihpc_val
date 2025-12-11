94db3b873eeee9918685f829fca1eb9b30d5bd92
junli-nv <junliz@nvidia.com>
2025-12-11 16:08:29 +0800

    Update run.sh

--- validation/mnubergemm/run.sh
+++ validation/mnubergemm/run.sh
@@ -3,14 +3,30 @@
 
 module load slurm
 hosts=(
-$(scontrol show hostname $(sinfo -al|grep 'reserved.*junli_val'|awk '{print $NF}'))
+$(scontrol show hostname $(sinfo -al|grep 'idle '|awk '{print $NF}'))
 )
+
+# for i in {0..9}
+# do
+#   for h in ${hosts[*]}
+#   do
+#     sbatch --reservation=junli_val \
+#       -N 1 \
+#       -w "${h}" \
+#       -t 2:30:00 \
+#       --requeue \
+#       --dependency=singleton \
+#       --job-name=MNUBERGEMM-${h} \
+#       --output=${USER}-MNUBERGEMEM-${h}-%j.txt \
+#       mnubergemm.sbatch
+#   done
+# done
 
 for i in {0..9}
 do
   for h in ${hosts[*]}
   do
-    sbatch --reservation=junli_val \
+    sbatch --wait \
       -N 1 \
       -w "${h}" \
       -t 2:30:00 \
@@ -18,6 +34,7 @@ do
       --dependency=singleton \
       --job-name=MNUBERGEMM-${h} \
       --output=${USER}-MNUBERGEMEM-${h}-%j.txt \
-      mnubergemm.sbatch
+      mnubergemm.sbatch &
   done
+  wait
 done
